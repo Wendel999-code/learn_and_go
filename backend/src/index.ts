6ad873468aac9e26838d4d726fbadb8399606e06
Wendel Sabayo <wendelsabayo999@gmail.com>
2025-09-03 09:51:58 +0800

    fix backend url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,16 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "https://learn-and-go-1.onrender.com/", // deployed frontend
+      "http://localhost:5173", // for local dev
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
